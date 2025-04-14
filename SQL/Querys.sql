ba5d28f0ff2877194597cf6a8848950047a5f577
wallacee20 <wallace_almeidajj@hotmail.com>
2025-04-14 17:42:37 -0300

    Atualiza arquivos SQL: remove Locadora e adiciona Querys

---
+++ SQL/Querys.sql
@@ -0,0 +1,17 @@
+-- Basic SQL Queries
+
+-- 1 - seleciona todos os dados da tabela cliente
+SELECT * FROM cliente;
+
+-- 2 - seleciona todos os dados da tabela cliente onde o nome é igual a 'Lucas'
+SELECT * FROM cliente WHERE nome = 'Lucas';
+
+-- 3 - seleciona todos os dados da tabela cliente onde o nome é igual a 'Lucas' e o sobrenome é igual a 'Silva' 
+SELECT * FROM cliente WHERE nome = 'Lucas' AND sobrenome = 'Silva';
+
+-- 4 - seleciona todos os dados da tabela cliente onde o nome é igual a 'Lucas' ou o sobrenome é igual a 'Silva'
+SELECT * FROM cliente WHERE nome = 'Lucas' OR sobrenome = 'Silva';
+
+-- 5 - seleciona todos os dados da tabela cliente onde o nome é diferente de 'Lucas'
+SELECT * FROM cliente WHERE nome <> 'Lucas';
+
